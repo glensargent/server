6c667aa15f20e97c0ab710f74a9e45cf32351a28
Glen Sargent <glensarg@hotmail.co.uk>
2020-09-21 18:57:21 +0100

    shut down workers on drop

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ impl<F: FnOnce()> FnBox for F {
     }
 }
 
-type Job = Box<FnBox + Send + 'static>;
+type Job = Box<dyn FnBox + Send + 'static>;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
@@ -48,9 +48,20 @@ impl ThreadPool {
         }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -61,9 +72,10 @@ impl Worker {
                 job.call_box();
             }
         });
+
         Worker {
             id,
-            thread,
+            thread: Some(thread),
         }
     }
 }
